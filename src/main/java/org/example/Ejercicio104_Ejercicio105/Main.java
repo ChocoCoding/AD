fc8062329bcdd900eee861e6f6446f243d3fa1f7
Gonzalo Campos Dominguez <gonzalo.dominguez.cd@gmail.com>
2023-09-14 14:31:13 +0200

    Ejercicios terminados del 101 al 105

---
+++ src/main/java/org/example/Ejercicio104_Ejercicio105/Main.java
@@ -0,0 +1,8 @@
+package org.example.Ejercicio104_Ejercicio105;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("El aforo de la piscina es de: " + Ejercicio_Piscina.calcularAforoPiscina() + " personas");
+        Ejercicio_Piscina.reservarPorFranja();
+    }
+}
---
+++ src/main/java/org/example/Ejercicio104_Ejercicio105/Parcela.java
@@ -0,0 +1,27 @@
+package org.example.Ejercicio104_Ejercicio105;
+
+public class Parcela {
+    private double longitud;
+    private double anchura;
+
+    public Parcela(double longitud, double anchura) {
+        this.longitud = longitud;
+        this.anchura = anchura;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public double getAnchura() {
+        return anchura;
+    }
+
+    public void setAnchura(double anchura) {
+        this.anchura = anchura;
+    }
+}
