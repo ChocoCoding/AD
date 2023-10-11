b3d019b7ee41e7241f0bcb770a0fa5530a81427c
Gonzalo Campos Dominguez <gonzalo.dominguezcd@gmail.com>
2023-10-11 16:05:18 +0200

    Actualizaciones para el pull

---
+++ src/main/java/org/example/primera_evaluacion/unidad2/ejercicio215/ManejoDeDOM.java
@@ -0,0 +1,17 @@
+package org.example.primera_evaluacion.unidad2.ejercicio215;
+
+import org.xml.sax.SAXException;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class ManejoDeDOM {
+    public static void main(String[] args) throws IOException, SAXException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance() ;
+        //DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        //Document doc = dBuilder.parse( new File( "fitler.xml" ));
+    }
+}
